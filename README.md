2ffff33595288089ecbb9156bdf2ff37b00f20c3
victormutuku <victor.mutuku@strathmore.edu>
2022-04-01 19:36:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# shop_app
+ This is a shopping mobile app that allows users to not only buy products but also sell them.
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Shop App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:AppBar(title: const Text('Shop App')),
+        body: const Center(child: Text('Shop App'),));
+  }
+}
