843da71f8aca73769a92d45f32963867a48e5971
victormutuku <victor.mutuku@strathmore.edu>
2022-04-02 13:32:39 +0300

    Theming and Styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 import '../screens/products_overview_screen.dart';
 
 void main() {
@@ -12,7 +13,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shop App',
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato,'
+      ),
       home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName:(context) => const ProductDetailScreen(),
+      },
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,28 +1,49 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imageUrl;
-  const ProductItem(this.id, this.title, this.imageUrl, {Key? key}) : super(key: key);
+  const ProductItem(this.id, this.title, this.imageUrl, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-        child: FadeInImage.assetNetwork(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: GridTile(
+        child: GestureDetector(
+          child: FadeInImage.assetNetwork(
             placeholder: 'assets/images/image_placeholder.jpg',
-            image: imageUrl, 
+            image: imageUrl,
             fit: BoxFit.cover,
           ),
+          onTap: () {
+            Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: id,
+              );
+          },
+        ),
         footer: GridTileBar(
-              backgroundColor: Colors.black.withOpacity(0.6),
-              leading: IconButton(icon: Icon(Icons.favorite_border), onPressed: null,),
-              title: Text(
-                  title,
-                  textAlign: TextAlign.center,
-                ),
-              trailing: IconButton(icon: Icon(Icons.shopping_cart), onPressed: null,),
-            ),
-      );
+          backgroundColor: Colors.black.withOpacity(0.6),
+          leading: IconButton(
+            icon: Icon(Icons.favorite_border,
+                color: Theme.of(context).accentColor),
+            onPressed: null,
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon:
+                Icon(Icons.shopping_cart, color: Theme.of(context).accentColor),
+            onPressed: null,
+          ),
+        ),
+      ),
+    );
   }
 }
