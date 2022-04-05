3fbcd2e3c3c17393aebd106e0acdd92d1e674789
victormutuku <victor.mutuku@strathmore.edu>
2022-04-05 14:26:08 +0300

    Added a listener to the product detail page

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -4,7 +4,7 @@ import '../models/product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
-      Product(
+    Product(
       id: 'p1',
       title: 'Red Shirt',
       description: 'A red shirt - it is pretty red!',
@@ -40,6 +40,10 @@ class Products with ChangeNotifier {
 
   List<Product> get items {
     return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((product) => product.id == id);
   }
 
   void addProducts() {
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/productdetail';
@@ -9,8 +11,9 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-      appBar: AppBar(title: Text('title')),
+      appBar: AppBar(title: Text(loadedProduct.title!)),
     );
   }
 }
