f0d2cc7da2195bc8bf817eac0b63cc9fccb0b9f5
Victor Mutuku <victor.mutuku@strathmore.edu>
2022-06-17 19:15:59 +0300

    Making Card Item Dismissible
    This is accomplished by using the Dismissible widget which is built into Flutter. It can be customized for different outputs

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -48,6 +48,7 @@ class CartScreen extends StatelessWidget {
                 itemBuilder: ((context, i) => 
                     CartItem(
                         cart.items.values.toList()[i].id,
+                        cart.items.keys.toList()[i],
                         cart.items.values.toList()[i].price,
                         cart.items.values.toList()[i].quantity,
                         cart.items.values.toList()[i].title)),
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart' as pc;
 
 class CartItem extends StatelessWidget {
   final String? id;
+  final String? productId;
   final double? price;
   final int? quantity;
   final String? title;
 
-  const CartItem(this.id, this.price, this.quantity, this.title, {Key? key}): super(key: key);
+  const CartItem(this.id, this.productId, this.price, this.quantity, this.title, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: FittedBox(child: Text('\$$price')),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of <pc.Cart>(context, listen: false).removeItem(productId!);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: FittedBox(child: Text('\$$price')),
+              ),
+              backgroundColor: Theme.of(context).primaryColor,
             ),
-            backgroundColor: Theme.of(context).primaryColor,
+            title: Text(title!),
+            subtitle: Text('Total: \$${(price! * quantity!)}'),
+            trailing: Text('x $quantity'),
           ),
-          title: Text(title!),
-          subtitle: Text('Total: \$${(price! * quantity!)}'),
-          trailing: Text('x $quantity'),
         ),
       ),
     );
