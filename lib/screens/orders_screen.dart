6435551f3fcf5de0edae008d7be9444e3f89f96c
Victor Mutuku <victor.mutuku@strathmore.edu>
2022-08-26 11:19:09 +0300

    Using the Future builder widget.
    Allows us to call a Future within the widget tree.

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,49 +6,36 @@ import '../widgets/loading_spinner.dart';
 import '../widgets/order_item.dart';
 import '../screens/side_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-  @override
-  void initState() {
-    // Future.delayed(Duration.zero).then((_) async {
-
-    _isLoading = true;
-
-    Provider.of<Orders>(context, listen: false)
-        .fetchAndSetOrders()
-        .then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-
-    // });
-
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(title: const Text('Your Orders')),
-      drawer: const SideDrawer(),
-      body: _isLoading
-          ? const LoadingSpinner(
-              text: 'Retrieving Data',
-            )
-          : ListView.builder(
-              itemBuilder: ((context, i) => OrderItem(orderData.orders[i])),
-              itemCount: orderData.orders.length,
-            ),
-    );
+        appBar: AppBar(title: const Text('Your Orders')),
+        drawer: const SideDrawer(),
+        body: FutureBuilder(
+            future:
+                Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+            builder: ((context, dataSnapshot) {
+              if (dataSnapshot.connectionState == ConnectionState.waiting) {
+                return const LoadingSpinner(
+                  text: 'Retrieving Data',
+                );
+              }
+              if (dataSnapshot.error != null) {
+                return const Center(
+                  child: Text('An error occurred'),
+                );
+              } else {
+                return Consumer<Orders>(
+                    builder: (ctx, orderData, child) => ListView.builder(
+                          itemBuilder: ((context, i) =>
+                              OrderItem(orderData.orders[i])),
+                          itemCount: orderData.orders.length,
+                        ));
+              }
+            })));
   }
 }
